acf62a5a7bbf184a50ebdce6ea279532395c9da7
will-robertson0 <wj.robertson24@gmail.com>
2025-08-18 14:37:32 -0500

    add and delete note functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,44 @@
 // import React from "react"; // not needed because .jsx
 import { createRoot } from "react-dom/client";
-import NotesList from './components/NotesList';
+import NotesList from "./components/NotesList";
 import { useState } from "react";
-import { nanoid } from 'nanoid';
- 
+import { nanoid } from "nanoid";
+import Search from './components/Search';
+
 const App = () => {
-    const [notes, setNotes] = useState([{
-            id: nanoid(),
-            text: "lorem ipsum somethin",
-            date: "13/32/2027"
-        },
-        {
-            id: nanoid(),
-            text: "lorem ipsum second",
-            date: "14/32/2027"
-        },
-        {
-            id: nanoid(),
-            text: "lorem ipsum third",
-            date: "15/32/2027"
-        },
-    ]);
+  const [notes, setNotes] = useState([
+    {
+      id: nanoid(),
+      text: "lorem ipsum somethin",
+      date: "13/32/2027",
+    },
+    {
+      id: nanoid(),
+      text: "lorem ipsum second",
+      date: "14/32/2027",
+    },
+    {
+      id: nanoid(),
+      text: "lorem ipsum third",
+      date: "15/32/2027",
+    },
+  ]);
+
+  const addNote = (text) => {
+    const date = new Date();
+    const newNote = {
+      id: nanoid(),
+      text: text,
+      date: date.toLocaleDateString(),
+    };
+    const newNotes = [...notes, newNote];
+    setNotes(newNotes);
+  };
+
+  const deleteNote = (id) => {
+    const newNotes = notes.filter((note) => note.id !== id);
+    setNotes(newNotes);
+  };
 
   return (
     // <nav class="main-navbar">
@@ -28,10 +46,18 @@ const App = () => {
     //             <img src="./media/back-button.webp" alt="clickable back button image" />
     //         </a>
     // </nav>
-        <div className="container">
-            <NotesList notes={notes}/>
-        </div>
+    <div className="container">
+      <Search/>
+      <NotesList
+        notes={notes}
+        handleAddNote={addNote}
+        handleDeleteNote={deleteNote}
+      />
+    </div>
   );
+  // "prop drilling":
+  // handleAddNote is passed as a prop to NotesList ^^^ and then to AddNote so that
+  // the child element AddNote can change the state of the parent element App
 };
 
 const container = document.getElementById("root");
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,15 +1,19 @@
-import { MdDeleteForever } from 'react-icons/md';
+import { MdDeleteForever } from "react-icons/md";
 
-const Note = ({ id, text, date }) => {
-    return (
-        <div className="note">
-            <span>{text}</span>
-            <div className="note-footer">
-                <small>{date}</small>
-                <MdDeleteForever className="delete-icon" size="1.3em" />
-            </div>
-        </div>
-    );
-}
+const Note = ({ id, text, date, handleDeleteNote }) => {
+  return (
+    <div className="note">
+      <span>{text}</span>
+      <div className="note-footer">
+        <small>{date}</small>
+        <MdDeleteForever
+          onClick={() => handleDeleteNote(id)}
+          className="delete-icon"
+          size="1.3em"
+        />
+      </div>
+    </div>
+  );
+};
 
 export default Note;
